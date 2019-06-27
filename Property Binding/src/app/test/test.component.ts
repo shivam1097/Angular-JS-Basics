649b6f177a0c7070a5efbdbaeeefaea85c5f66ac
Shivam1097 <shivam.dev1097@gmail.com>
2019-06-27 20:37:22 +0530

    Initial Commit

---
+++ Property Binding/src/app/test/test.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test',
+  templateUrl: './test.component.html',
+  styleUrls: ['./test.component.css']
+})
+export class TestComponent implements OnInit {
+
+  myId="sample";
+  isDisabled=true;
+
+  constructor() { }
+  ngOnInit() {
+  }
+
+  public toggleInput(){
+    if(this.isDisabled){
+      this.isDisabled=false;
+    }else{
+      this.isDisabled=true;
+    }
+  }
+
+}
+
+
