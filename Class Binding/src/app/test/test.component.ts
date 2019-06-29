4e358ed59a63122eb8a4a9781ca50e2f833c203b
Shivam1097 <shivam.dev1097@gmail.com>
2019-06-29 11:39:49 +0530

    Initial commit

---
+++ Class Binding/src/app/test/test.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test',
+  templateUrl: './test.component.html',
+  styleUrls: ['./test.component.css']
+})
+export class TestComponent implements OnInit {
+
+  successClass="success";
+  dangerClass="danger";
+  specialClass="special";
+
+  // We can also use boolean values to decide whether a class will be assigned or not.
+  isError=false;
+  isSpecial=true;  //There is special syntax to use this, refer to html file.
+
+  // Creating object of class which can be assigned directly
+
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
